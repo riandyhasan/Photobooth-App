fb05ee8e58c7d46f56a8ecc23bfde86c4c8073c5
riandyhasan <riandyhsn@gmail.com>
2023-10-31 21:39:54 +0700

    feat: modal in scan

--- src/pages/scan/index.js
+++ src/pages/scan/index.js
@@ -1,6 +1,8 @@
 const $ = require("jquery");
 const Html5QrcodeScanner = require("html5-qrcode").Html5QrcodeScanner;
 
+const close = document.querySelector("#close");
+
 const scanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 300 });
 
 const modal = document.querySelector("#qr-modal");
@@ -12,7 +14,6 @@ const dashboard = document.querySelector("#reader__dashboard");
 modal.querySelector(".modal-content").appendChild(dashboard);
 
 function onScanSuccess(decodedText, decodedResult) {
-  console.log(decodedText);
   window.location.href = "../payment/index.html";
 
   // You can choose to stop scanning after a successful scan
@@ -23,24 +24,6 @@ function onScanFailure(error) {
   // console.error(error);
 }
 
-navigator.mediaDevices
-  .getUserMedia({ video: true })
-  .then(function (stream) {
-    modal.style.display = "none";
-    stream.getTracks().forEach((track) => track.stop());
-  })
-  .catch(function (error) {
-    if (
-      error.name === "NotAllowedError" ||
-      error.name === "PermissionDeniedError"
-    ) {
-      console.log("Camera access denied");
-    } else if (
-      error.name === "NotFoundError" ||
-      error.name === "SourceUnavailableError"
-    ) {
-      console.log("No camera found or the camera is not available");
-    } else {
-      console.error("Error while checking camera access:", error);
-    }
-  });
+close.addEventListener("click", () => {
+  modal.style.display = "none";
+});
