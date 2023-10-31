47501f632501bcad5f493122af8061188fd745f7
riandyhasan <riandyhsn@gmail.com>
2023-10-31 14:48:40 +0700

    feat: camera selection

--- src/pages/scan/index.js
+++ src/pages/scan/index.js
@@ -3,7 +3,13 @@ const Html5QrcodeScanner = require("html5-qrcode").Html5QrcodeScanner;
 
 const scanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 300 });
 
+const modal = document.querySelector("#qr-modal");
+
 scanner.render(onScanSuccess, onScanFailure);
+
+const dashboard = document.querySelector("#reader__dashboard");
+
+modal.querySelector(".modal-content").appendChild(dashboard);
 
 function onScanSuccess(decodedText, decodedResult) {
   console.log(decodedText);
@@ -16,3 +22,25 @@ function onScanSuccess(decodedText, decodedResult) {
 function onScanFailure(error) {
   // console.error(error);
 }
+
+navigator.mediaDevices
+  .getUserMedia({ video: true })
+  .then(function (stream) {
+    stream.getTracks().forEach((track) => track.stop());
+  })
+  .catch(function (error) {
+    modal.style.display = "block";
+    if (
+      error.name === "NotAllowedError" ||
+      error.name === "PermissionDeniedError"
+    ) {
+      console.log("Camera access denied");
+    } else if (
+      error.name === "NotFoundError" ||
+      error.name === "SourceUnavailableError"
+    ) {
+      console.log("No camera found or the camera is not available");
+    } else {
+      console.error("Error while checking camera access:", error);
+    }
+  });
